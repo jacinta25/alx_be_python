9ebf074853088e56de08db3fed42fc24cd0ee269
jacinta25 <mwendejacinta25@gmail.com>
2024-09-12 22:26:28 +0300

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#define variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculate simple interest
+interest = principal * rate * time
+
+#print the results
+print(f"The Simple interst is: {interest}")
