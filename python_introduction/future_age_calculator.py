b1f2ad51c3fcb69f4f8af4a2058583636781e06c
jacinta25 <mwendejacinta25@gmail.com>
2024-09-20 22:53:37 +0300

    Weather Recommendation Program

--- python_introduction/future_age_calculator.py
+++
@@ -1,6 +0,0 @@
-# define variables
-current_age = int(input("How old are you? "))
-
-
-age = current_age + 27
-print(f"In 2050, you will be {age} years old.")
--- python_introduction/hours_to_seconds.py
+++
@@ -1,8 +0,0 @@
-#define variables
-hours = 2
-
-#calculations
-seconds = hours * 3600
-
-#print result
-print(f"{hours} hour(s) is {seconds} seconds.")
