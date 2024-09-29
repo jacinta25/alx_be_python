cfc16d141eec090da9ba0f97317e4a126bffdd1a
jacinta25 <mwendejacinta25@gmail.com>
2024-09-29 11:04:31 +0300

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
-        return num2 - num1
+        return num1 - num2
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
